110af0b4ae63f6c082fdf7d524807bbf80b02b67
Vtorov Sergei <ftoroff@gmail.com>
2022-04-25 22:40:06 +0300

    Lesson3 tasks added

---
+++ src/main/java/lesson3/Magic8ball.java
@@ -0,0 +1,41 @@
+package lesson3;
+
+public class Magic8ball {
+
+    /**
+     * Сделать магического шара восьмёрки. Фразы должны находиться в виде строки в ArrayList.
+     * Для реализации задачи использовать классы Random и ArrayList.
+     * Метод getResult() должен отдавать строковое значение со случайной фразой из массива (ArrayList) строк (фраз).
+     * При запускпе программа должна выводить на экран случайную фразу из списка.
+     * phrases.add("As I see it, yes");
+     * "Ask again later"
+     * "Better not tell you now"
+     * "Cannot predict now"
+     * "Concentrate and ask again"
+     * "Don’t count on it"
+     * "It is certain"
+     * "It is decidedly so"
+     * "Most likely"
+     * "My reply is no"
+     * "My sources say no"
+     * "Outlook good"
+     * "Outlook not so good"
+     * "Reply hazy try again"
+     * "Signs point to yes"
+     * "Very doubtful"
+     * "Without a doubt"
+     * "Yes"
+     * "Yes, definitely"
+     * "You may rely on it"
+     */
+
+    public static void main(String[] args) {
+        System.out.println(getResult());
+    }
+
+    public static String getResult() {
+        //Написать реализацию метода тут
+        return "";
+    }
+
+}
---
+++ src/main/java/lesson3/RandomLesson.java
@@ -0,0 +1,20 @@
+package lesson3;
+
+public class RandomLesson {
+
+    /**
+     * Используя класс Random, сделать реализацию метода, который будет выдавать случайную фразу из ArrayList.
+     * Фраз в ArrayList должно быть четыре, фразы могут быть любые.
+     * При запуске программа должна выводить на экран одну случайную фразу из четырех строк, находящихся в ArrayList.
+     * Реализацию метода, производящего вывод случайной фразы делать в методе getRandomPhrase()
+     */
+
+    public static void main(String[] args) {
+        System.out.println(getRandomPhrase());
+    }
+
+    public static String getRandomPhrase() {
+        // Писать код тут
+        return "";
+    }
+}
