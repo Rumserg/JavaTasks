296f32e0c82ccadf46d8ca5704f1af751f5e99a0
Vtorov Sergei <ftoroff@gmail.com>
2022-05-04 21:28:41 +0300

    Yet another pair of tasks for lesson4 has been added

---
+++ src/main/java/lesson4/MinAndMax.java
@@ -0,0 +1,35 @@
+package lesson4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Написать два метода.
+ * 1. Метод для поиска минимального числа из четырех чисел.
+ * 1.1. Метод должен принимать в качестве параметра четыре числа.
+ * 1.2. Метод должен находить минимальное из них.
+ * 1.3. Метод должен возвращать минимальное число в качестве результата.
+ * 1.4. Для реализации метода необходимо использовать Collections (List, сортировки)
+ * 2. Метод для поиска максимального числа из пяти чисел.
+ * 2.1. Метод должен принимать в качестве параметра пять чисел.
+ * 2.2. Метод должен находить максимальное из них.
+ * 2.3. Метод должен возвращать максимальное число в качестве результата.
+ * 2.4. Для реализации метода необходимо использовать Collections (List, сортировки)
+ */
+
+public class MinAndMax {
+    public static void main(String[] args) {
+        System.out.println(minNumber(700,654,908,7));
+        System.out.println(maxNumber(700,614,893,900,590));
+
+    }
+    public static int minNumber(int a,int b,int c,int d) {
+
+        return 0;
+    }
+     public static int maxNumber(int a, int b, int c, int d, int e) {
+
+         return 0;
+     }
+}
---
+++ src/main/java/lesson4/PrintNtimes.java
@@ -0,0 +1,19 @@
+package lesson4;
+
+/**
+ * Написать метод print, который при вызове возвращает строку введенную в параметр метода N раз.
+ * Параметров будет два: 1) Фараза 2) число, которое нужно будет вызвать фразу.
+ * Подсказка: нужно использовать StringBuilder и цикл for.
+ * В main вызвать метод (распечатать).
+ */
+
+public class PrintNtimes {
+    public static void main(String[] args) {
+        System.out.println(print("Phrase you would like to print",10));
+    }
+
+    public static String print(String phrase,int number){
+
+        return "";
+    }
+}
